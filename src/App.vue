8c880e6a1a78ef9982339fe161421a2381662b74
heshifang <617002215@qq.com>
2018-01-26 09:49:53 +0800

    no message

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    created: function () {
+      if (!localStorage.getItem('OPENID')) {
+        let openId = location.href.split('?')[1].split('=')[1]
+        localStorage.setItem('OPENID', openId)
+      }
+    }
+  }
+</script>
+
+<style lang="less">
+  @import '~vux/src/styles/reset.less';
+
+  html {
+    height: 100%;
+    width: 100%;
+  }
+
+  body {
+    height: 100%;
+    width: 100%;
+    overflow-y: hidden;
+    position: fixed;
+  }
+
+  #app {
+    height: 100%;
+  }
+
+  body {
+    background-color: #fbf9fe;
+  }
+</style>
